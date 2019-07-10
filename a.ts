b883589a550aed427a61326f8277f1959e9cfdcf
Cataunt <2639515493@qq.com>
2019-07-10 18:01:45 +0800

    参数新特性

--- a.ts
+++ a.ts
@@ -7,16 +7,16 @@ ccc`
 
 // 字符串模板
 
-// var myname = "zhang san"
-// var getName = function() {
-//     return "zhang u"
-// }
+var myname = "zhang san"
+var getName = function() {
+    return "zhang u"
+}
 
-// console.log(`hello ${myname}`)
-// console.log(`<div>
-// <span>${myname}</span>
-// <span>${getName}</span>
-// </div>`)
+console.log(`hello ${myname}`)
+console.log(`<div>
+<span>${myname}</span>
+<span>${getName}</span>
+</div>`)
 
 // 自动拆分字符串
 
@@ -33,3 +33,49 @@ var getAge = function () {
 }
 
 test`hello my name is ${myname}, i'm ${getAge()}`
+
+
+// 参数类型(基本类型、函数参数类型、自定义类型)
+var myname:string ="zhang san";
+
+var alias:any = "xixi";
+alias = 0
+
+var age:number = 12;
+var man:boolean = false;
+
+function test2(name: string) :string {
+    return "";
+}
+
+test2("Cataunt")
+
+class Person {
+    name: string | undefined;
+    age: number | undefined
+}
+
+var zhang:Person = new Person ()
+zhang.name = "zhangggg";
+zhang.age = 16;
+
+// 参数默认值
+var my:string = "zhangss"
+function yest(a:string, b:string, c:string) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+yest("xxx","yyy","cccc")
+
+
+function jest(a:string, b:string, c:string="jojo") {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+jest("xxx","yyy")
+
+// 可选参数
--- out/a.js
+++ out/a.js
@@ -6,15 +6,12 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
 };
 var content = "\naaa \nbbb \nccc";
 // 字符串模板
-// var myname = "zhang san"
-// var getName = function() {
-//     return "zhang u"
-// }
-// console.log(`hello ${myname}`)
-// console.log(`<div>
-// <span>${myname}</span>
-// <span>${getName}</span>
-// </div>`)
+var myname = "zhang san";
+var getName = function () {
+    return "zhang u";
+};
+console.log("hello " + myname);
+console.log("<div>\n<span>" + myname + "</span>\n<span>" + getName + "</span>\n</div>");
 // 自动拆分字符串
 function test(template, name, age) {
     console.log(template);
@@ -25,4 +22,40 @@ var myname = "zhai zhang";
 var getAge = function () {
     return 18;
 };
-test(__makeTemplateObject(["hello my name is ", ", i'm ", ""], ["hello my name is ", ", i'm ", ""]), myname, getAge());
+test(__makeTemplateObject(["hello my name is ", ", i'm ", ""], ["hello my name is ", ", i'm ", ""
+    // 参数类型(基本类型、函数参数类型、自定义类型)
+]), myname, getAge());
+// 参数类型(基本类型、函数参数类型、自定义类型)
+var myname = "zhang san";
+var alias = "xixi";
+alias = 0;
+var age = 12;
+var man = false;
+function test2(name) {
+    return "";
+}
+test2("Cataunt");
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var zhang = new Person();
+zhang.name = "zhangggg";
+zhang.age = 16;
+// 参数默认值
+var my = "zhangss";
+function yest(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+yest("xxx", "yyy", "cccc");
+function jest(a, b, c) {
+    if (c === void 0) { c = "jojo"; }
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+jest("xxx", "yyy");
+// 可选参数
